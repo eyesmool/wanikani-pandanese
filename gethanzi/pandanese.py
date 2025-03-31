7303072d18f4ea729bdfcbb215c346f05d075d4e
eyesmool <z5513417@ad.unsw.edu.au>
2025-03-31 22:35:38 +1100

    Add files

--- gethanzi/pandanese.py
+++ gethanzi/pandanese.py
@@ -5,7 +5,84 @@ import argparse;
 from bs4 import BeautifulSoup;
 import requests;
 import pyperclip
+import concurrent.futures
+from collections import OrderedDict
 
+
+def format_output(char, dic):
+    formattedOutput = f"<div><b>hanzi</b>: {char}</div>\n<div><br></div>\n\n"
+    terminalOutput = f"{color.BOLD}{color.CYAN}hanzi{color.END}{color.END}: {char}\n\n"
+    for key, value in dic.items():
+        formattedOutput += f"<div><b>{key}</b>: {value}</div>\n<div><br></div>\n\n"
+        terminalOutput += f"{color.BOLD}{color.CYAN}{key}{color.END}{color.END}: {value}\n\n"
+
+    print(terminalOutput)
+
+    pyperclip.copy(formattedOutput)
+
+    print(color.BOLD + color.GREEN + "Copied to clipboard" + color.END)
+    
+    return formattedOutput
+    
+def get_substrings(hanzi):
+    # TODO: can make more efficient if 希望 exists then so do the individual components
+    substrings = [hanzi[i:j] for i in range(len(hanzi)) for j in range(i+1, len(hanzi)+1)]
+    substrings.sort(key=len, reverse=True)
+    print(substrings)
+
+    subStrOrderedDict = OrderedDict()
+    dic = {}
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_subStr = {executor.submit(does_whole_hanzi_exist, subStr): subStr for subStr in substrings}
+
+        for future in concurrent.futures.as_completed(future_to_subStr):
+            subStr = future_to_subStr[future]
+            try:
+                exists, hanziReq = future.result()
+                if exists:
+                    subStrOrderedDict[subStr] = None  # Using OrderedDict to maintain order
+                    dic[subStr] = hanziReq
+            except Exception as e:
+                print(f"Error processing {subStr}: {e}")
+
+    return list(subStrOrderedDict.keys()), dic
+def does_whole_hanzi_exist(char):
+    url = f"https://www.pandanese.com/search?q={char}"
+    # print(f"Checking if {char} exists ")
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    dic = {}
+    labels = ['meaning', 'mneumonic', 'reading', 'reading mneumonic']
+    count = 0
+    for tag in soup.find_all('div', class_='definition_value'):
+        text = tag.text.strip()
+        dic[labels[count]] = text
+        count += 1
+        if count == 4:
+            break
+
+    if dic.get('meaning') == None:
+        return False, dic
+    # print(color.GREEN + f"{char}" + "exists" + color.END)
+    return True, dic
+def req_format_and_print(char):
+    url = f"https://www.pandanese.com/search?q={char}"
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    dic = {}
+    labels = ['meaning', 'mneumonic', 'reading', 'reading mneumonic']
+    count = 0
+    for tag in soup.find_all('div', class_='definition_value'):
+        text = tag.text.strip()
+        dic[labels[count]] = text
+        count += 1
+        if count == 4:
+            break
+
+    if dic.get('meaning') is None:
+        print(color.RED + "No results found" + color.END)
+        return 0
+
+    return format_output(char, dic)
 
 class color:
    PURPLE = '\033[95m'
@@ -23,36 +100,92 @@ class color:
 parser = argparse.ArgumentParser(description='Get menumonics from pandanese.com')
 
 parser.add_argument('hanzi', metavar='C', type=str, help='the char to lookup')
+parser.add_argument('-r', '--recursive', action='store_true', help='recursively looks up hanzi')
+parser.add_argument('-l', '--lookup', action='store_true', help='looks up all chars')
+parser.add_argument('-f', '--format', action='store_true', help='formats output as flexbox for anki cards')
+# TODO: add a -s search option that searches for substrings and checks if they exist against a dictionary
+
+
+
 
 args = parser.parse_args()
+if args.format and args.lookup and args.recursive:    
+    formattedOutput = """<div class="flex">"""
+    if does_whole_hanzi_exist(args.hanzi):
+        formattedOutput += """<div class="child">"""
+        formattedOutput += f"""<div class="hanzi">{args.hanzi}</div> <br>"""
+        formattedOutput += req_format_and_print(args.hanzi)
+        formattedOutput += """</div>"""
+    for char in args.hanzi:
+        formattedOutput += """<div class="child">"""
+        formattedOutput += f"""<div class="hanzi">{char}</div> <br>"""
+        formattedOutput += req_format_and_print(char)
+        formattedOutput += """</div>"""
+    formattedOutput += """</div>"""
+    pyperclip.copy(formattedOutput)
+    print(color.BOLD + color.GREEN + "Formatted output copied to clipboard" + color.END)
+elif args.format and args.recursive:
+    subStrSet, dic = get_substrings(args.hanzi)
+    formattedOutput = ""
+    formattedOutput += """<div class="flex">"""
+    seen = set()
+    for hanzi in dic:
+        if hanzi in seen:
+            continue
+        formattedOutput += """<div class="child">"""
+        formattedOutput += f"""<div class="hanzi">{hanzi}</div> <br>"""
+        formattedOutput += format_output(hanzi, dic[hanzi])
+        formattedOutput += """</div>"""
+        if len(hanzi) > 1:
+            for char in hanzi:
+                if char in seen: continue
+                formattedOutput += """<div class="child">"""
+                formattedOutput += f"""<div class="hanzi">{char}</div> <br>"""
+                formattedOutput += format_output(char, dic[char])
+                formattedOutput += """</div>"""
+                seen.add(char)
+                
+        # for char in hanzi:
+        #     formattedOutput += """<div class="child">"""
+        #     formattedOutput += f"""<div class="hanzi">{char}</div> <br>"""
+        #     if (dic[char]):
+        #         formattedOutput += format_output(hanzi, dic[char])
+        #     else:
+        #         formattedOutput += req_format_and_print(char)
+        #     formattedOutput += """</div>"""
+    formattedOutput += """</div>"""
+    pyperclip.copy(formattedOutput)
+    print(color.BOLD + color.GREEN + "Formatted output copied to clipboard" + color.END)
+elif args.format and args.lookup:
+    formattedOutput = """<div class="flex">"""
+    for char in args.hanzi:
+        formattedOutput += """<div class="child">"""
+        formattedOutput += f"""<div class="hanzi">{char}</div> <br>"""
+        formattedOutput += req_format_and_print(char)
+        formattedOutput += """</div>"""
+    formattedOutput += """</div>"""
+    pyperclip.copy(formattedOutput)
+    print(color.BOLD + color.GREEN + "Formatted output copied to clipboard" + color.END)
+elif args.format:    
+    formattedOutput = """<div class="flex">"""
+    formattedOutput += """<div class="child">"""
+    formattedOutput += f"""<div class="hanzi">{args.hanzi}</div> <br>"""
+    formattedOutput += req_format_and_print(args.hanzi)
+    formattedOutput += """</div>"""
+    pyperclip.copy(formattedOutput)
+    print(color.BOLD + color.GREEN + "Formatted output copied to clipboard" + color.END)
+elif args.lookup:    
+    for char in args.hanzi:
+        req_format_and_print(char)
+elif args.recursive:
+    subStrSet = get_substrings(args.hanzi)
+    for subStr in subStrSet:
+        req_format_and_print(subStr)
+        for char in subStr:
+            req_format_and_print(char)
+else:    
+    req_format_and_print(args.hanzi)
+    
 
-url = f"https://www.pandanese.com/search?q={args.hanzi}"
-soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-dic = {}
-labels = ['meaning', 'mneumonic', 'reading', 'reading mneumonic']
-count = 0
-for tag in soup.find_all('div', class_='definition_value'):
-    text = tag.text.strip()
-    dic[labels[count]] = text
-    count += 1
-    if count == 4:
-        break
-
-if dic.get('meaning') == None:
-    print(color.RED + "No results found" + color.END)
-    sys.exit(1)
-
-print()
-formattedOutput = ''
-terminalOutput = ''
-for key, value in dic.items():
-    formattedOutput += f"<div><b>{key}</b>: {value}</div>\n<div><br></div>\n\n"
-    terminalOutput += f"{color.BOLD}{color.CYAN}{key}{color.END}{color.END}: {value}\n\n"
-
-print(terminalOutput)
-
-pyperclip.copy(formattedOutput)
-
-print(color.BOLD + color.GREEN + "Copied to clipboard" + color.END)
 
 
--- wanikani/wanikani.py
+++ wanikani/wanikani.py
@@ -13,6 +13,62 @@ def listToDict(lst):
         res_dict[lst[i]] = lst[i + 1]
     return res_dict
 
+def fetch_kanji_data(char):
+    url = f"https://www.wanikani.com/kanji/{char}"
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    dic = {}
+    radicals_text = ''
+    cleaned_text = ''
+    # Get radical information
+    for tag in soup.find_all('section', class_='subject-section subject-section--components'):
+        radicals_text += re.sub(r'\s+', ' ', tag.text).strip()
+
+    radicals_text = re.sub(r'Radical\sCombination','', radicals_text).strip()
+    # print(radicals_text)
+    # this is a bit of a hack with the ｲ and L character, but it works for now
+    pattern = r'([\u4e00-\u9fff\u30a0-\u30ff\u2e80-\u2effｲL㠯]+)\s+([a-zA-Z\s]+)'
+    matches = re.findall(pattern, radicals_text)
+    radicalsArray = [item for match in matches for item in match]
+    radicalDict = listToDict(radicalsArray)
+
+    for section in soup.find_all('section', class_='subject-section__subsection'):
+        cleaned_text += re.sub(r'\s+', ' ', section.text).strip() + '\n'
+
+    if (cleaned_text == ''): 
+        print(color.RED + f"kanji {char} not found" + color.END)
+        sys.exit()
+
+    primary_pattern = r"Primary\s(.+?)(?:\sAlternatives|\sWord\sType|\n)"
+    primary = re.search(primary_pattern, cleaned_text)
+    if primary:
+        dic['primary'] = primary.group(1)
+
+    meaning_mnemonic_pattern = r"Mnemonic\s(.+)\n"
+    meaning_mnemonic = re.search(meaning_mnemonic_pattern, cleaned_text)
+    if meaning_mnemonic:
+        dic['meaning mnemonic'] = meaning_mnemonic.group(1)
+
+    onyomi_pattern = r"On’yomi\s(.+)\sKun’yomi"
+    onyomi = re.search(onyomi_pattern, cleaned_text)
+    if onyomi:
+        dic['onyomi'] = onyomi.group(1).strip()
+
+    kunyomi_pattern = r"Kun’yomi\s(.+)\sNanori"
+    kunyomi = re.search(kunyomi_pattern, cleaned_text)
+    if kunyomi:
+        dic['kunyomi'] = kunyomi.group(1).strip()
+
+    nanori_pattern = r"Nanori\s(.+)\n"
+    nanori = re.search(nanori_pattern, cleaned_text)
+    if nanori:
+        dic['nanori'] = nanori.group(1).strip()
+        
+    reading_mnemonic_pattern = r"Nanori.+\sMnemonic\s(.+)\n"
+    reading_mnemonic = re.search(reading_mnemonic_pattern, cleaned_text)
+    if reading_mnemonic:
+        dic['reading mnemonic'] = reading_mnemonic.group(1).strip()
+
+    return dic, radicalDict, url
 class color:
    PURPLE = '\033[95m'
    CYAN = '\033[96m'
@@ -29,13 +85,25 @@ parser = argparse.ArgumentParser(description='Get menumonics from wanikani.com')
 
 parser.add_argument('type', metavar='type', type=str, help='vocab / kanji')
 parser.add_argument('char', metavar='char', type=str, help='the char to lookup')
+parser.add_argument('-r', '--recursive', action='store_true', help='recursively looks up kanji')
+parser.add_argument('-b', '--breakdown', action='store_true', help='breaks down non hira/kana chars')
+parser.add_argument('-l', '--lookup', action='store_true', help='looks up every kanji')
+parser.add_argument('-f', '--format', action='store_true', help='formats for anki flexbox')
+
+
+# TODO: have a --radical flag that looks adds radicals
+
+
 
 
 args = parser.parse_args()
 
-def format_and_print(char, color, dic, radicalsDict=None):
-    formattedOutput = f'<div><b>{char}</b></div>\n'
-    terminalOutput = f'{color.BOLD}{color.PURPLE}character: {color.END}{color.END}{char}\n\n'
+def format_and_print(char, color, dic, radicalsDict=None, radical=None):
+    formattedOutput = f'<div class="kanji"><b>{char}</b></div>\n'
+    if (radical):
+        terminalOutput = f'{color.BOLD}{color.PURPLE}radical: {color.END}{color.END}{char}\n\n'
+    else:
+        terminalOutput = f'{color.BOLD}{color.PURPLE}character: {color.END}{color.END}{char}\n\n'
     if (radicalsDict):
         formattedOutput += "<div><b>Radicals</b>:</div>\n"
         terminalOutput += f"{color.BOLD}{color.PURPLE}radicals{color.END}{color.END}:\n"
@@ -46,6 +114,16 @@ def format_and_print(char, color, dic, radicalsDict=None):
         terminalOutput += "\n"
     
     for key, value in dic.items():
+        if key == 'breakdown': 
+            breakDown = ''
+            formatBreakDown = '<div>'
+            for k, v in value.items():
+                breakDown += f"[{k}: {v}] "
+                formatBreakDown += f"<li>[{k}: {v}]</li>"
+            formatBreakDown += '</div>'
+            formattedOutput += f"<div><b>{key}</b>: {formatBreakDown}</div>\n<div><br></div>\n\n"
+            terminalOutput += f"{color.BOLD}{color.PURPLE}{key}{color.END}{color.END}: {breakDown}\n\n"
+            continue
         formattedOutput += f"<div><b>{key}</b>: {value}</div>\n<div><br></div>\n\n"
         terminalOutput += f"{color.BOLD}{color.PURPLE}{key}{color.END}{color.END}: {value}\n\n"
     
@@ -54,20 +132,31 @@ def format_and_print(char, color, dic, radicalsDict=None):
     pyperclip.copy(formattedOutput)
 
     print(color.BOLD + color.GREEN + "Copied to clipboard" + color.END)
+    
+    return formattedOutput
 
 if args.type == 'vocab':
     url = f"https://www.wanikani.com/vocabulary/{args.char}"
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     dic = {}
     cleaned_text = ''
+    rawBreakdown = ''
     for section in soup.find_all('section', class_='subject-section__subsection'):
         cleaned_text += re.sub(r'\s+', ' ', section.text).strip() + '\n'
     
     if (cleaned_text == ''): 
         print(color.RED + f"{args.type} not found" + color.END)
         sys.exit()
-
-    # print(cleaned_text)
+    
+    for section in soup.find_all('div', class_='subject-character-grid'):
+        rawBreakdown += re.sub(r'\s+', ' ', section.text).strip() + '\n'
+        # print(rawBreakdown)
+        kanjiSplit = re.findall(r'[一-龥]+|[ぁ-ん]+|[A-Za-z]+(?: [A-Za-z]+)?', rawBreakdown)
+        kanjiBreakdown = {}
+        for i in range(0, len(kanjiSplit), 3):
+            kanjiBreakdown[kanjiSplit[i]] = kanjiSplit[i + 1] + ' ' + kanjiSplit[i + 2]
+        dic['breakdown'] = kanjiBreakdown
+    # print(cleaned_text) 
     
     primary_pattern = r"Primary\s(.+?)(?:\sAlternatives|\sWord\sType)"
     primary = re.search(primary_pattern, cleaned_text)
@@ -89,68 +178,69 @@ if args.type == 'vocab':
     if pronunciation:
         dic['pronunciation'] = pronunciation.group(1)
     
-    mnemonic_pattern = r"\)\sExplanation\s(.+)\sContext\sSentences"
+    # mnemonic_pattern = r"\)\sExplanation\s(.+)\sContext\sSentences"
+    mnemonic_pattern = r"\)\sExplanation\s(.+)\n"
     mnemonic = re.search(mnemonic_pattern, cleaned_text)
     if mnemonic:
         dic['mnemonic'] = mnemonic.group(1)
     
-    
-    format_and_print(args.char, color, dic)
+    if args.recursive and args.format and 'breakdown' in dic:
+        formattedOutput = """<div class="flex">"""
+        formattedOutput += """<div class="child">"""
+        formattedOutput += format_and_print(args.char, color, dic)
+        formattedOutput += """</div>"""
+        for k, v in dic['breakdown'].items():
+            formattedOutput += """<div class="child">"""
+            kanji_dic, radicalDict, kanji_url = fetch_kanji_data(k)
+            formattedOutput += format_and_print(k, color, kanji_dic, radicalDict)
+            formattedOutput += """</div>"""
+            pyperclip.copy(formattedOutput)
+        formattedOutput += """</div>"""
+    elif args.recursive and 'breakdown' in dic:
+        formattedOutput = format_and_print(args.char, color, dic)
+        for k, v in dic['breakdown'].items():
+            kanji_dic, radicalDict, kanji_url = fetch_kanji_data(k)
+            formattedOutput += format_and_print(k, color, kanji_dic, radicalDict)
+            pyperclip.copy(formattedOutput)
+    else:
+        formattedOutput = format_and_print(args.char, color, dic)
+        pyperclip.copy(formattedOutput)
+
+    print(color.BOLD + color.YELLOW + f"Source: {url} " + color.END)
 
 if args.type == 'kanji':
-    url = f"https://www.wanikani.com/kanji/{args.char}"
+    if args.lookup or args.recursive:
+        formattedOutput = """<div class="flex">"""
+        for char in args.char:
+            dic, radicalDict, url = fetch_kanji_data(char)
+            formattedOutput += """<div class="child">"""
+            # formattedOutput += f"""<div class="hanzi">{char}</div> <br>"""
+            formattedOutput += format_and_print(char, color, dic, radicalDict)
+            formattedOutput += """</div>"""
+            print(color.BOLD + color.YELLOW + f"Source: {url} " + color.END)
+        formattedOutput += """</div>"""
+        pyperclip.copy(formattedOutput)
+else:
+        dic, radicalDict, url = fetch_kanji_data(args.char)
+        format_and_print(args.char, color, dic, radicalDict)
+        print(color.BOLD + color.YELLOW + f"Source: {url} " + color.END)
+
+if args.type == 'radical':
+    # use radical text from radicalDict i.e. (穴: Hole) to make 
+    #     url = f"https://www.wanikani.com/radicals/hole"
+    url = f"https://www.wanikani.com/radicals/{args.char}"
     soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     dic = {}
-    cleaned_text = ''
     radicals_text = ''
-    # Get radical information
-    for tag in soup.find_all('section', class_='subject-section subject-section--components'):
+    for tag in soup.find_all('section', class_='subject-section__content'):
         radicals_text += re.sub(r'\s+', ' ', tag.text).strip()
-
-    radicals_text = re.sub(r'Radical\sCombination','', radicals_text).strip()
+    # to do: finish this
     # print(radicals_text)
-    # this is a bit of a hack with the ｲ character, but it works for now
-    pattern = r'([\u4e00-\u9fff\u30a0-\u30ff\u2e80-\u2effｲ]+)\s+([a-zA-Z\s]+)'
-    matches = re.findall(pattern, radicals_text)
-    radicalsArray = [item for match in matches for item in match]
-    radicalDict = listToDict(radicalsArray)
-
-    for section in soup.find_all('section', class_='subject-section__subsection'):
-        cleaned_text += re.sub(r'\s+', ' ', section.text).strip() + '\n'
+    mnemonic_pattern = r"Mnemonic\s(.+?)[^\x00-\x7F]"
+    mnemonic = re.search(mnemonic_pattern, radicals_text)
+    if mnemonic:
+        dic['mnemonic'] = mnemonic.group(1).strip()
     
-    if (cleaned_text == ''): 
-        print(color.RED + f"{args.type} not found" + color.END)
-        sys.exit()
-    
-    primary_pattern = r"Primary\s(.+?)(?:\sAlternatives|\sWord\sType|\n)"
-    primary = re.search(primary_pattern, cleaned_text)
-    if primary:
-        dic['primary'] = primary.group(1)
-    
-    meaning_mnemonic_pattern = r"Mnemonic\s(.+)\n"
-    meaning_mnemonic = re.search(meaning_mnemonic_pattern, cleaned_text)
-    if meaning_mnemonic:
-        dic['meaning mnemonic'] = meaning_mnemonic.group(1)
-
-    onyomi_pattern = r"On’yomi\s(.+)\sKun’yomi"
-    onyomi = re.search(onyomi_pattern, cleaned_text)
-    if onyomi:
-        dic['onyomi'] = onyomi.group(1).strip()
-    
-    kunyomi_pattern = r"Kun’yomi\s(.+)\sNanori"
-    kunyomi = re.search(kunyomi_pattern, cleaned_text)
-    if kunyomi:
-        dic['kunyomi'] = kunyomi.group(1).strip()
-    
-    nanori_pattern = r"Nanori\s(.+)\n"
-    nanori = re.search(nanori_pattern, cleaned_text)
-    if nanori:
-        dic['nanori'] = nanori.group(1).strip()
-        
-    reading_mnemonic_pattern = r"Nanori.+\sMnemonic\s(.+)\n"
-    reading_mnemonic = re.search(reading_mnemonic_pattern, cleaned_text)
-    if reading_mnemonic:
-        dic['reading mnemonic'] = reading_mnemonic.group(1).strip()
-        
-    format_and_print(args.char, color, dic, radicalDict)
+    format_and_print(args.char, color, dic, None, args.char)
     print(color.BOLD + color.YELLOW + f"Source: {url} " + color.END)
+
